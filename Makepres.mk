2f53200a6cd03ef6d18bc94b992bae753e97b7fa
Marcus Tang <tmc9031@gmail.com>
2014-07-07 00:08:57 +0800

    new: zlib premake project

--- Makepres.mk
+++ Makepres.mk
@@ -1,16 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
-###################### helloc ######################
+###################### makec ######################
 include $(CLEAR_VARS)
 
-LOCAL_PREMAKE_MODULE := makec
+LOCAL_MODULE := makec
 
-LOCAL_PREMAKE_EXECUTABLE := build/c-make
-
-LOCAL_PREMAKE_CONFIG_FILE := configure
-LOCAL_PREMAKE_CONFIG_CMD := $(LOCAL_PREMAKE_CONFIG)
+#LOCAL_PREMAKE_CONFIG_CMD :=
 
 LOCAL_PREMAKE_MAKE_FILE := makefile
 LOCAL_PREMAKE_MAKE_DIR := $(LOCAL_PATH)/build
+
+LOCAL_PREMAKE_MAKE_TARGET := all
+LOCAL_PREMAKE_MAKE_ARGS :=
+
+LOCAL_PREMAKE_MAKE_CLEAN := clean
 
 include $(BUILD_SYSTEM)/premake.mk
